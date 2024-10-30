7595cef8ca2392052c346597b3eda4c48f0f471c
timur galiullin <timur.galiullin24@outlook.com>
2024-10-30 08:22:19 +0300

    Добавьте файлы проекта.

---
+++ ConstructionCalculation.Client/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ConstructionCalculation.Client
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
