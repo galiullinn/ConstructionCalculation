ef0a98eec6838f2e450d8aafad1886fddbca5cc6
timur galiullin <timur.galiullin24@outlook.com>
2024-10-30 08:32:45 +0300

    Реализовано взаимодействие MainWindowViewModel с MainWindow

---
+++ ConstructionCalculation.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ConstructionCalculation.Client.ViewModels.Base;
+
+namespace ConstructionCalculation.Client.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Главная страница";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+    }
+}
