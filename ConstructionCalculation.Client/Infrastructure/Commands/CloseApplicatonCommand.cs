4416a907ed6baf201a0c2c61e5a7f596697557bd
timur galiullin <timur.galiullin24@outlook.com>
2024-10-30 08:54:28 +0300

    Реализована комманда для завершения работы приложения (протестирована работа команд)

---
+++ ConstructionCalculation.Client/Infrastructure/Commands/CloseApplicatonCommand.cs
@@ -0,0 +1,12 @@
+﻿using ConstructionCalculation.Client.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace ConstructionCalculation.Client.Infrastructure.Commands
+{
+    internal class CloseApplicatonCommand : Command
+    {
+        public override bool CanExecute(object? parameter) => true;
+
+        public override void Execute(object? parameter) => Application.Current.Shutdown();
+    }
+}
